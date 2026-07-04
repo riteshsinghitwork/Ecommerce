85eb2278449780d389dc65d8d29ce7b4f9e42cfa
riteshsinghitwork <riteshsingh.it.work@gmail.com>
2026-07-04 10:58:31 +0530

    Ecommerce project setup

---
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -0,0 +1,39 @@
+package com.ecommerce.project.model;
+
+public class Category {
+
+    private Long categoryId;
+    private String categoryName;
+
+    // Default constructor (Required)
+    public Category() {
+    }
+
+    public Category(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", categoryName='" + categoryName + '\'' +
+                '}';
+    }
+}
