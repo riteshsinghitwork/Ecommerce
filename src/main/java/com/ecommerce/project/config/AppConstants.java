db5b6065db5b44a8e2f0e9bf23e813ff5032632a
riteshsinghitwork <riteshsingh.it.work@gmail.com>
2026-07-19 17:56:43 +0530

    Diagram and response

--- src/main/java/com/ecommerce/project/config/AppConstants.java
+++ src/main/java/com/ecommerce/project/config/AppConstants.java
@@ -3,5 +3,6 @@ package com.ecommerce.project.config;
 public class AppConstants {
     public static final String DEFAULT_PAGE_NUMBER = "0";
     public static final String DEFAULT_PAGE_SIZE = "10";
-
+    public static final String DEFAULT_SORT_BY = "categoryId";
+    public static final String DEFAULT_SORT_ORDER = "asc";
 }
--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -24,8 +24,10 @@ public class CategoryController {
 
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getCategories(@RequestParam(name = "pageNumber", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) Integer pageNumber,
-                                                          @RequestParam(name = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) Integer pageSize) {
-        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
+                                                          @RequestParam(name = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) Integer pageSize,
+                                                          @RequestParam(name = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+                                                          @RequestParam(name = "sortOrder", defaultValue = AppConstants.DEFAULT_SORT_ORDER, required = false) String sortOrder) {
+        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize, sortBy, sortOrder);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
