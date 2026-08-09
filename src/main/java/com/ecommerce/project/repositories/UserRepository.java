1be681e5df7cf717f58695f72442cf651e2598b0
riteshsinghitwork <riteshsingh.it.work@gmail.com>
2026-08-09 09:03:41 +0530

    Spring security auth controller, provider, management

---
+++ src/main/java/com/ecommerce/project/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+}
